eb721deba4cd664a039c758acb876f865a7cb4b9
DrClefSCP <114330195+DrClefSCP@users.noreply.github.com>
2022-09-26 23:35:11 -0300

    Bug de repeticion arreglado
    Se soluciona el que se envíe muchas veces el mensaje (esto por no usar Coroutines)

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,14 +1,30 @@
 using Exiled.API.Features;
+using Exiled.Events.EventArgs;
 using MEC;
+using System.Collections.Generic;
 
 namespace Test1
 {
     public class EventHandlers
     {
+        private CoroutineHandle coroutine;
         public void OnRoundStarted()
         {
             Timing.CallDelayed(Plugin.Instance.Config.Delay1, () => Cassie.MessageTranslated(Plugin.Instance.Config.CassieMessage1, Plugin.Instance.Config.CassieMessageTranslated1));
             Timing.CallDelayed(Plugin.Instance.Config.Delay2, () => Cassie.MessageTranslated(Plugin.Instance.Config.CassieMessage2, Plugin.Instance.Config.CassieMessageTranslated2));
+            coroutine = Timing.RunCoroutine(StartMessages());
+        }
+        public void RoundEndedEvent(RoundEndedEventArgs ev)
+        {
+            Timing.KillCoroutines(coroutine);
+            Cassie.Clear();
+        }
+        public IEnumerator<float> StartMessages()
+        {
+            yield return Timing.WaitForSeconds(Plugin.Instance.Config.Delay1);
+            Log.Info(Plugin.Instance.Config.CassieMessageTranslated1);
+            yield return Timing.WaitForSeconds(Plugin.Instance.Config.Delay2 - Plugin.Instance.Config.Delay1);
+            Log.Info(Plugin.Instance.Config.CassieMessageTranslated2);
         }
     }
 }
